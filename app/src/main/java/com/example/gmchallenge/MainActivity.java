e9c640ddcef78d615fbbadc49f390384b6fd89c3
Luis Aguirre (Luis) <luis.aguirre@gm.com>
2020-05-10 07:27:20 -0400

    Add missing condition

--- app/src/main/java/com/example/gmchallenge/MainActivity.java
+++ app/src/main/java/com/example/gmchallenge/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE || getSmallestScreenWidthDp() > 943) {
             setLandscapeView();
         } else {
             setPortraitView();
